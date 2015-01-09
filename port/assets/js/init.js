6907248fa2230038d0c86876d0f89a1086ad94e7
nmoya <nikolasmoya@gmail.com>
2015-01-08 22:53:11 -0200

    Links style fixed

--- port/assets/js/init.js
+++ port/assets/js/init.js
@@ -1,5 +1,21 @@
 //This file needs to be loaded after jquery
 $(function() {
-	$("header").load("header.html");
-	$("footer").load("footer.html");
+	function get_last_url_segment(url) {
+		return url.substr(url.lastIndexOf('/') + 1);
+	}
+	$("header").load("header.html", function() {
+		$("footer").load("footer.html", function() {
+			arr = $(".fixedElement a");
+			page = get_last_url_segment(document.URL);
+			if (page == '')
+				page = "index.html";
+			for (i = 0; i < arr.length; i++) {
+				if (page == get_last_url_segment(arr[i].href)) {
+					arr[i].classList.add("active-link");
+					break;
+				}
+			}
+		});
+	});
+
 });
