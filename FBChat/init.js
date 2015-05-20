3deed9fe224256ddf3971bbd1b6bf62eb755d7bd
nmoya <nikolasmoya@gmail.com>
2015-05-19 21:17:40 -0300

    Fetching all the messages is working

--- FBChat/init.js
+++ FBChat/init.js
@@ -6,28 +6,35 @@ function getAllConversations(request_url, user_name, callback)
 {
   FB.api(request_url, function (response)
   {
-    console.log(response);
-    if (response && response.error)
+    // console.log(response);
+    if (response.error)
     {
-      alert(response.error.message);
-      alert("Waiting 60 seconds");
-      setTimeout(getAllConversations(request_url, user_name, callback), 60000);
-    }
-    for (var i = 0; i < response.data.length; i++)
-    {
-      if (response.data[i].to.data.length == 2)
+      console.log(response.error.message);
+      console.log("Waiting 60 seconds");
+      setTimeout(function ()
       {
-        if (response.data[i].to.data[1].name != user_name)
+        console.log("Trying again");
+        getAllConversations(request_url, user_name, callback)
+      }, 60000);
+    }
+    else
+    {
+      for (var i = 0; i < response.data.length; i++)
+      {
+        if (response.data[i].to.data.length == 2)
         {
-          // console.log(response.data[i]);
-          listOfConversations.push(new Conversation(response.data[i].id, user_name, response.data[i].to.data[1].name))
+          if (response.data[i].to.data[1].name != user_name)
+          {
+            // console.log(response.data[i]);
+            listOfConversations.push(new Conversation(response.data[i].id, user_name, response.data[i].to.data[1].name))
+          }
         }
       }
+      if (response.paging)
+        getAllConversations(response.paging.next, user_name, callback)
+      else
+        callback();
     }
-    if (response.paging)
-      getAllConversations(response.paging.next, user_name, callback)
-    else
-      callback();
   });
 }
 
@@ -35,15 +42,17 @@ function getAllMessages(request_url, callback)
 {
   FB.api(request_url, function (response)
   {
-    if (response && response.error)
+    if (response.error)
     {
-      alert(response.error.message);
-      alert("Waiting 60 seconds to continue getting all messages");
-      setTimeout(getAllMessages(request_url, callback), 60000);
+      console.log(response.error.message);
+      console.log("Waiting 60 seconds to continue getting all messages");
+      setTimeout(function ()
+      {
+        getAllMessages(request_url, callback)
+      }, 60000);
     }
     else
     {
-      console.log(response);
       if (!response.comments)
         callback();
       for (var i = 0; i < response.comments.data.length; i++)
@@ -55,18 +64,52 @@ function getAllMessages(request_url, callback)
       }
       if (response.comments.paging)
       {
-        refreshHTML();
-        getAllMessages(response.comments.paging.next, callback);
+        refreshHTML(response.comments.data.length);
+        getAllMessagesPaging(response.comments.paging.next, callback);
       }
     }
   });
 }
 
-function refreshHTML()
+function getAllMessagesPaging(request_url, callback)
+{
+  FB.api(request_url, function (response)
+  {
+    if (response.error)
+    {
+      console.log(response.error.message);
+      console.log("Waiting 60 seconds to continue getting all messages");
+      setTimeout(function ()
+      {
+        getAllMessagesPaging(request_url, callback)
+      }, 60000);
+    }
+    else
+    {
+      // console.log(response);
+      for (var i = 0; i < response.data.length; i++)
+      {
+        listOfMessages.push(new Message(response.data[i].id,
+          response.data[i].from.name,
+          response.data[i].message,
+          response.data[i].created_time));
+      }
+      if (response.paging)
+      {
+        refreshHTML(response.data.length);
+        getAllMessagesPaging(response.paging.next, callback);
+      }
+      else
+        callback();
+    }
+  });
+}
+
+function refreshHTML(increment)
 {
   var counter = document.getElementById("progress").innerHTML;
   counter = parseInt(counter);
-  counter += 22;
+  counter += increment;
   document.getElementById("progress").innerHTML = counter;
 }
 
@@ -75,7 +118,14 @@ function handlerGenerateButton()
   var select = document.getElementById("conversations");
   var conversationId = select[select.selectedIndex].value;
   var to = select[select.selectedIndex].innerHTML;
+
+  var counter = document.getElementById("progress");
+  var table = document.getElementById("messagesTable");
+  table.innerHTML = "";
+  counter.innerHTML = 0;
+
   // 185506144849518
+  console.log("Starting");
   getAllMessages("/" + conversationId, computeStatistics);
 }
 
