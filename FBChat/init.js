5f985b38d76c5b5ee2a29d46d5ae5758ad609ce3
nmoya <nikolasmoya@gmail.com>
2015-05-26 03:31:34 -0300

    Front end foundations built

--- FBChat/init.js
+++ FBChat/init.js
@@ -1,30 +1,89 @@
+/* Facebook stuff */
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '760742574044608',
+    cookie: true, // enable cookies to allow the server to access 
+    // the session
+    xfbml: true, // parse social plugins on this page
+    version: 'v2.2' // use version 2.2
+  });
+
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+
+};
+
+// Load the SDK asynchronously
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function checkLoginState() {
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+}
+
+function statusChangeCallback(response) {
+  if (response.status === 'connected') {
+    main(); // Main is in init.js
+  } else if (response.status === 'not_authorized') {
+    document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
+  } else {
+    document.getElementById('status').innerHTML = 'Please log ' + 'into Facebook.';
+  }
+}
+
+var hideTimeout = 500;
 var listOfConversations = [];
 var listOfMessages = [];
 var user_name = null;
+$(document).ready(function () {
+  $("#typed").typed({
+    strings: ["........"],
+    typeSpeed: 0,
+    loop: true,
+    typeSpeed: 150,
+    startDelay: 0,
+    backSpeed: 0,
+    backDelay: 500,
+    showCursor: false,
+  });
+  document.getElementById("generateStats").addEventListener("click", handlerGenerateButton);
+  // $(".step1").hide();
+  $(".step2").hide();
+  $(".step3").hide();
+  $(".step4").hide();
+});
 
-function getAllConversations(request_url, user_name, callback)
-{
-  FB.api(request_url, function (response)
-  {
+function nextSimulator() {
+  $(".step2").hide(hideTimeout);
+  $(".step3").show();
+}
+
+
+function getAllConversations(request_url, user_name, callback) {
+  FB.api(request_url, function (response) {
     // console.log(response);
-    if (response.error)
-    {
+    if (response.error) {
+      $("#status").html("Due to heavy usage, our API has been limited by Facebook. We will keep trying, please leave this window open.");
       console.log(response.error.message);
       console.log("Waiting 60 seconds");
-      setTimeout(function ()
-      {
+      setTimeout(function () {
         console.log("Trying again");
         getAllConversations(request_url, user_name, callback)
       }, 60000);
-    }
-    else
-    {
-      for (var i = 0; i < response.data.length; i++)
-      {
-        if (response.data[i].to.data.length == 2)
-        {
-          if (response.data[i].to.data[1].name != user_name)
-          {
+    } else {
+      $("#status").html("");
+      for (var i = 0; i < response.data.length; i++) {
+        if (response.data[i].to.data.length == 2) {
+          if (response.data[i].to.data[1].name != user_name) {
             // console.log(response.data[i]);
             listOfConversations.push(new Conversation(response.data[i].id, user_name, response.data[i].to.data[1].name))
           }
@@ -38,83 +97,71 @@ function getAllConversations(request_url, user_name, callback)
   });
 }
 
-function getAllMessages(request_url, callback)
-{
-  FB.api(request_url, function (response)
-  {
-    if (response.error)
-    {
+function getAllMessages(request_url, callback) {
+  FB.api(request_url, function (response) {
+    if (response.error) {
       console.log(response.error.message);
+      $("#status").html("Due to heavy usage, our API has been limited by Facebook. We will keep trying, please leave this window open.");
       console.log("Waiting 60 seconds to continue getting all messages");
-      setTimeout(function ()
-      {
+      setTimeout(function () {
         getAllMessages(request_url, callback)
       }, 60000);
-    }
-    else
-    {
+    } else {
+      $("#status").html("");
       if (!response.comments)
         callback();
-      for (var i = 0; i < response.comments.data.length; i++)
-      {
+      for (var i = 0; i < response.comments.data.length; i++) {
         listOfMessages.push(new Message(response.comments.data[i].id,
           response.comments.data[i].from.name,
           response.comments.data[i].message,
           response.comments.data[i].created_time));
       }
-      if (response.comments.paging)
-      {
+      if (response.comments.paging) {
         refreshHTML(response.comments.data.length);
-        getAllMessagesPaging(response.comments.paging.next, callback);
+        getAllMessagesPaging(response.comments.paging.next, callback, response.comments.data.length);
       }
     }
   });
 }
 
-function getAllMessagesPaging(request_url, callback)
-{
-  FB.api(request_url, function (response)
-  {
-    if (response.error)
-    {
+function getAllMessagesPaging(request_url, callback, progress) {
+  FB.api(request_url, function (response) {
+    if (response.error) {
       console.log(response.error.message);
+      $("#status").html("Due to heavy usage, our API has been limited by Facebook. We will keep trying, please leave this window open.");
       console.log("Waiting 60 seconds to continue getting all messages");
-      setTimeout(function ()
-      {
-        getAllMessagesPaging(request_url, callback)
+      setTimeout(function () {
+        getAllMessagesPaging(request_url, callback, progress)
       }, 60000);
-    }
-    else
-    {
+    } else {
+      $("#status").html("");
       // console.log(response);
-      for (var i = 0; i < response.data.length; i++)
-      {
+      for (var i = 0; i < response.data.length; i++) {
         listOfMessages.push(new Message(response.data[i].id,
           response.data[i].from.name,
           response.data[i].message,
           response.data[i].created_time));
       }
-      if (response.paging)
-      {
+      if (response.paging && progress < 1000) {
+        progress += response.data.length
         refreshHTML(response.data.length);
-        getAllMessagesPaging(response.paging.next, callback);
-      }
-      else
+        getAllMessagesPaging(response.paging.next, callback, progress);
+      } else
         callback();
     }
   });
 }
 
-function refreshHTML(increment)
-{
+function refreshHTML(increment) {
   var counter = document.getElementById("progress").innerHTML;
   counter = parseInt(counter);
   counter += increment;
   document.getElementById("progress").innerHTML = counter;
 }
 
-function handlerGenerateButton()
-{
+function handlerGenerateButton() {
+  $(".step2").hide(hideTimeout);
+  $(".step3").show();
   var select = document.getElementById("conversations");
   var conversationId = select[select.selectedIndex].value;
   var to = select[select.selectedIndex].innerHTML;
@@ -129,19 +176,18 @@ function handlerGenerateButton()
   getAllMessages("/" + conversationId, computeStatistics);
 }
 
-function computeStatistics()
-{
+function computeStatistics() {
+  $(".step3").hide(hideTimeout);
+  $(".step4").show();
   console.log("callback called. computeStatistics");
   console.log("Number of messages: ");
   console.log(listOfMessages.length);
-  generateTable();
+  //generateTable();
 }
 
-function generateSelectOptions()
-{
+function generateSelectOptions() {
   var select = document.getElementById("conversations");
-  for (var i = 0; i < listOfConversations.length; i++)
-  {
+  for (var i = 0; i < listOfConversations.length; i++) {
     var option = document.createElement("option");
     option.value = listOfConversations[i].id;
     option.innerHTML = listOfConversations[i].to;
@@ -149,11 +195,9 @@ function generateSelectOptions()
   }
 }
 
-function generateTable()
-{
+function generateTable() {
   var table = document.getElementById("messagesTable");
-  for (var i = 0; i < listOfMessages.length; i++)
-  {
+  for (var i = 0; i < listOfMessages.length; i++) {
     var row = table.insertRow(-1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
@@ -164,12 +208,11 @@ function generateTable()
   }
 }
 
-function main()
-{
+function main() {
   var user_id = null;
-  document.getElementById("generateStats").addEventListener("click", handlerGenerateButton);
-  FB.api('/me', function (response)
-  {
+  $(".step1").hide(hideTimeout);
+  $(".step2").show();
+  FB.api('/me', function (response) {
     user_id = response.id;
     user_name = response.name;
     console.log(user_id, user_name);
