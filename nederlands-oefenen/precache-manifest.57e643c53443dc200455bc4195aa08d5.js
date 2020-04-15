5e8f530c6338bba9bf857d2053ca4bae7f7c6aa0
Nikolas Moya <moya@work.co>
2020-04-15 12:37:30 +0200

    Create sample react app for nederlands practice

---
+++ nederlands-oefenen/precache-manifest.57e643c53443dc200455bc4195aa08d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c04f388554dee34c1ea35c50ab963bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ac511cb74533e350705",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "57b7fcb8755e8f204109",
+    "url": "./static/js/2.6848c33e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6848c33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac511cb74533e350705",
+    "url": "./static/js/main.7ce6a69f.chunk.js"
+  },
+  {
+    "revision": "efca43860a45fc980574",
+    "url": "./static/js/runtime-main.104f1bcf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
