e74dcbc40cc2d97f4ab03395fa512d6fc1758552
Nikolas Moya <moya@work.co>
2020-04-16 14:31:10 +0200

    Update deployed code

---
+++ nederlands-oefenen/precache-manifest.2e5de3ee57df618891505f39bc95d182.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9669e9bcd54bc1a0b6d124933e13cffe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "155c89636c4670fe8a3e",
+    "url": "./static/css/2.81e2c8a5.chunk.css"
+  },
+  {
+    "revision": "5001831dfa8a8f2cf24b",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "155c89636c4670fe8a3e",
+    "url": "./static/js/2.60ba01e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.60ba01e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5001831dfa8a8f2cf24b",
+    "url": "./static/js/main.3b6a731f.chunk.js"
+  },
+  {
+    "revision": "efca43860a45fc980574",
+    "url": "./static/js/runtime-main.104f1bcf.js"
+  }
+]);
--- nederlands-oefenen/service-worker.js
+++ nederlands-oefenen/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.57e643c53443dc200455bc4195aa08d5.js"
+  "./precache-manifest.2e5de3ee57df618891505f39bc95d182.js"
 );
 
 self.addEventListener('message', (event) => {
